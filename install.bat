a8fa5173c3cff09dcac6e2eeef14955a867a6aec
Useless <178414134+TheUselessCreator@users.noreply.github.com>
2026-09-08 21:24:32 -0400

    Update install.bat

--- install.bat
+++ install.bat
@@ -2,7 +2,7 @@
 setlocal enabledelayedexpansion
 chcp 65001 >nul 2>&1
 
-title ii's Stupid Menu Installer // [#---------] Getting directory
+title ii.menu Installer // [#---------] Getting directory
 color 0e
 
 set "steamPath1=C:\Program Files (x86)\Steam\steamapps\common\Gorilla Tag"
@@ -10,27 +10,27 @@ set "steamPath2=D:\SteamLibrary\steamapps\common\Gorilla Tag"
 set "steamPath3=C:\Program Files\Oculus\Software\Software\another-axiom-gorilla-tag"
 set "steamPath4=D:\Steam\steamapps\common\Gorilla Tag"
 
-if exist "!steamPath1!" (
-    set "gamePath=!steamPath1!"
+if exist "%steamPath1%" (
+    set "gamePath=%steamPath1%"
     goto :gotpath
 )
-if exist "!steamPath2!" (
-    set "gamePath=!steamPath2!"
+if exist "%steamPath2%" (
+    set "gamePath=%steamPath2%"
     goto :gotpath
 )
-if exist "!steamPath3!" (
-    set "gamePath=!steamPath3!"
+if exist "%steamPath3%" (
+    set "gamePath=%steamPath3%"
     goto :gotpath
 )
-if exist "!steamPath4!" (
-    set "gamePath=!steamPath4!"
+if exist "%steamPath4%" (
+    set "gamePath=%steamPath4%"
     goto :gotpath
 )
 
 color 0c
-set /p gamePath=Gorilla Tag directory not found. Enter it manually:
-set gamePath=%gamePath:"=%
-if not exist "!gamePath!" (
+set /p "gamePath=Gorilla Tag directory not found. Enter it manually: "
+set "gamePath=%gamePath:"=%"
+if not exist "%gamePath%" (
     echo Invalid directory.
     pause
     exit /b
@@ -39,25 +39,19 @@ if not exist "!gamePath!" (
 :gotpath
 color 0e
 cls
-title ii's Stupid Menu Installer // [###-------] Downloading BepInEx
+title ii.menu Installer // [###-------] Downloading BepInEx
 
 echo.
-echo   ████████ ██  █████  █████████   ███████████   █   ██████ █  █
-echo    ██   ██  █ █    █ █    ██    █    ██   █  █  ██ ██ █    ██ █
-echo    ██   ██  █ █    █ ████ ██    ████ ██   █  █  █ █ █ ███  █ ██
-echo    ██   ██  █ █    █ █    ██    █    ██   █  █  █   █ █    █  █
-echo   ████████   █ █████ ████ ██████ ████ ██████  █ █   ██████ █  █
-echo.
-echo        ii's Stupid Menu - Installer
-echo        github.com/iireborn/ii.Stupid.Menu
+echo   ii.menu Installer
+echo   github.com/iireborn/iis.Stupid.Menu
 echo.
 
-curl -L -f -# "https://github.com/BepInEx/BepInEx/releases/download/v5.4.23.4/BepInEx_win_x64_5.4.23.4.zip" -o BPNX54234.zip
+curl -L -f -# "https://github.com/BepInEx/BepInEx/releases/download/v5.4.23.4/BepInEx_win_x64_5.4.23.4.zip" -o "BPNX54234.zip"
 
 if errorlevel 1 (
     color 0c
     echo.
-    echo Failed to download BepInEx, check your internet connection.
+    echo Failed to download BepInEx. Check your internet connection.
     pause
     exit /b
 )
@@ -65,23 +59,23 @@ if errorlevel 1 (
 powershell -command "Expand-Archive -Path 'BPNX54234.zip' -DestinationPath '%gamePath%' -Force"
 
 cls
-title ii's Stupid Menu Installer // [####------] Creating directories
+title ii.menu Installer // [####------] Creating directories
 echo Creating BepInEx directories...
 if not exist "%gamePath%\BepInEx\config" mkdir "%gamePath%\BepInEx\config"
 if not exist "%gamePath%\BepInEx\plugins" mkdir "%gamePath%\BepInEx\plugins"
 
 cls
-title ii's Stupid Menu Installer // [#######---] Downloading menu
-echo Downloading latest release of ii's Stupid Menu...
+title ii.menu Installer // [#######---] Downloading menu
+echo Downloading latest release of ii.menu...
 
-for /f "tokens=*" %%i in ('powershell -Command "(Invoke-RestMethod -Uri 'https://api.github.com/repos/iireborn/iis.Stupid.Menu/releases/latest').assets | Where-Object { $_.name -like '*.dll' } | Select-Object -First 1 -ExpandProperty browser_download_url"') do (
-    set pluginUrl=%%i
+for /f "tokens=*" %%i in ('powershell -Command "(Invoke-RestMethod -Uri '"'"'https://api.github.com/repos/iireborn/iis.Stupid.Menu/releases/latest'"'"').assets | Where-Object { $_.name -like '"'"'*.dll'"'"' } | Select-Object -First 1 -ExpandProperty browser_download_url"') do (
+    set "pluginUrl=%%i"
 )
 
 if "%pluginUrl%"=="" (
     color 0c
     echo.
-    echo Failed to get latest release of menu, please report to Discord
+    echo Failed to get latest release. Please report to Discord.
     pause
     exit /b
 )
@@ -91,27 +85,20 @@ curl -L -f -# "%pluginUrl%" -o "%gamePath%\BepInEx\plugins\ii.s.Stupid.Menu.dll"
 if errorlevel 1 (
     color 0c
     echo.
-    echo Failed to download the menu, please report to Discord
+    echo Failed to download the menu. Please report to Discord.
     pause
     exit /b
 )
 
 cls
-title ii's Stupid Menu Installer // [##########] Finished
+title ii.menu Installer // [##########] Finished!
 
 echo.
-echo   ████████ ██  █████  █████████   ███████████   █   ██████ █  █
-echo    ██   ██  █ █    █ █    ██    █    ██   █  █  ██ ██ █    ██ █
-echo    ██   ██  █ █    █ ████ ██    ████ ██   █  █  █ █ █ ███  █ ██
-echo    ██   ██  █ █    █ █    ██    █    ██   █  █  █   █ █    █  █
-echo   ████████   █ █████ ████ ██████ ████ ██████  █ █   ██████ █  █
+echo   ii.menu Installer
 echo.
-echo        ii's Stupid Menu - Installer
+echo   Congratulations, ii.menu is installed!
 echo.
-
-echo Congratulations, you now have the menu!
-echo.
-echo Launch Gorilla Tag and the menu will load automatically.
+echo   Launch Gorilla Tag and the menu will load automatically.
 echo.
 del "BPNX54234.zip" >nul 2>&1
 pause
